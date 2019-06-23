32eb5efa8e85fa404ff6b1b68e2e90020aaa5177
elvis <yinchuang94@gmail.com>
2019-06-23 16:17:31 +0800

    add user and shout page

---
+++ app/controllers/shouts_controller.rb
@@ -0,0 +1,24 @@
+class ShoutsController < ApplicationController
+  def show
+      @shout = Shout.find(params[:id])
+  end
+
+  def create
+    shout =  current_user.shouts.create(shout_params)
+    redirect_to root_path, redirect_option_for(shout)
+  end
+
+  private
+
+  def shout_params
+    params.require(:shout).permit(:body)
+  end
+
+  def redirect_option_for(shout)
+    if shout.persisted?
+      { notice: "Shouted Successfully" }
+    else
+      { alert: "Could not shout" }
+    end
+  end
+end
